a37a994ca01023e2810c65e9704d25294a45a044
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-04-19 09:19:06 +0300

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+API_ID = os.getenv("API_ID", "19645327").strip()
+API_HASH = os.getenv("API_HASH", "92de937beb2f87db08df95bcca0ac2d6").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5801830106:AAHVe5BbKsJFNF9sRYSPIVLr80m0tCs_uU0").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/Tepthon")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
