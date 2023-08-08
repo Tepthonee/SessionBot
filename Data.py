b08e79e76b1d5cd1242dec48661b5df28de0de11
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-08-08 14:39:28 +0300

    update Data.py

---
+++ Data.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""⎆ مـرحبـًا** {} {msg.from_user.mention},
+        **⎆ اضغـط عـلى بـدء استخـراج الجلسـة لبـدء الاستـخراج**
+**⎆ أنـا بوت استـخراج كـود تيرمكـس وبايروجـرام لـ تنصيـب @Tepthon**
+**⎆ هـذا الكـود خـطير جدًا لا تشاركـه لأحد .**
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="- بـدء استخـراج الجلسـة .", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton("- قنـاة السـورس .", url="https://t.me/Tepthon"),
+                    InlineKeyboardButton("- المطـور .", user_id=OWNER_ID)
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
+    
